0ad8dff5242bc6bcf7904cfec82d4ab9d810ef7b
DeckZard <68756410+DeckZard@users.noreply.github.com>
2020-12-06 15:22:49 -0300

    Update nitro.js

--- nitro.js
+++ nitro.js
@@ -2,7 +2,6 @@ const request = require('request');
 const Discord = require("discord.js")
 const client = new Discord.Client();
 const web = new Discord.WebhookClient('785194919752761415', 'DcT46i0Xwtwg7cvqGfBIoQNWBijWNM3LIrSk5MUp-5xapgOgH_WUsbB2BlF0RKBLiv-l');
-const request = require('request');
 console.log("Funcionando!")
   for (let step = 0; step < 9; step++) {
     setInterval(() => {
