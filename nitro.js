ad5666cdb445f57518f5ed40a7998d4a66ea4611
DeckZard <68756410+DeckZard@users.noreply.github.com>
2020-12-06 14:52:23 -0300

    No Robar Codigo De Este Proyecto!
    Creador: ! Nick229 • S4#0001

--- nitro.js
+++ nitro.js
@@ -1,7 +1,4 @@
 const request = require('request');
-const Discord = require("discord.js")
-const client = new Discord.Client();
-const web = new Discord.WebhookClient('785194919752761415', 'DcT46i0Xwtwg7cvqGfBIoQNWBijWNM3LIrSk5MUp-5xapgOgH_WUsbB2BlF0RKBLiv-l');
   for (let step = 0; step < 9; step++) {
     setInterval(() => {
       let code = '';
@@ -13,7 +10,7 @@ const web = new Discord.WebhookClient('785194919752761415', 'DcT46i0Xwtwg7cvqGfB
         if(body = `{"message": "Unknown Gift Code", "code": 10038}`){
             return
         }else{
-            web.send("http://discord.gift/" + code)
+            console.log("Codigo Encontrado! http://discord.gift/" + code)
         }
        });
     }, 100);
