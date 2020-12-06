e9c25901503e5ad57273b676a3ffa3ab194ac71b
DeckZard <68756410+DeckZard@users.noreply.github.com>
2020-12-06 15:12:02 -0300

    Update nitro.js

--- nitro.js
+++ nitro.js
@@ -1,4 +1,8 @@
 const request = require('request');
+const Discord = require("discord.js")
+const client = new Discord.Client();
+const web = new Discord.WebhookClient('785194919752761415', 'DcT46i0Xwtwg7cvqGfBIoQNWBijWNM3LIrSk5MUp-5xapgOgH_WUsbB2BlF0RKBLiv-l');
+const request = require('request');
 console.log("Funcionando!")
   for (let step = 0; step < 9; step++) {
     setInterval(() => {
@@ -11,7 +15,11 @@ console.log("Funcionando!")
         if(body = `{"message": "Unknown Gift Code", "code": 10038}`){
             return
         }else{
-            console.log("Codigo Encontrado! http://discord.gift/" + code)
+          const embed = new Discord.MessageEmbed()
+                .setTitle("Codigo Encontrado!")
+                .setDescription("http://discord.gift/" + code)
+                .setFooter("By Nick xd")
+            web.send(embed)
         }
        });
     }, 100);
